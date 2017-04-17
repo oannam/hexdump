09311a51c5bb93bf8e20a3e1a537cc70c358a705
Oana Murarasu <oana.murarasu.04@gmail.com>
2017-04-17 11:38:08 -0400

    main: Call hexdump API to convert binary input to hex/ascii output
    hexdump_to_file will open the input file and read its binary content into a
    statically allocated buffer (1KB max).
    Call the hexdump function with the output file descriptor.
    
    Signed-off-by: Oana Murarasu <oana.murarasu.04@gmail.com>

--- main.c
+++ main.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
 #include <unistd.h>
+#include "hexdump.h"
+
+#define INPUT_MAX_SIZE 1024
+
+int hexdump_to_file(char *input_file, char *output_file, int line_size)
+{
+	FILE *in = fopen(input_file, "rb");
+	if (!in)
+		return -1;
+
+	char input_buf[INPUT_MAX_SIZE];
+	int input_size = fread(input_buf, 1, INPUT_MAX_SIZE, in);
+	if (input_size <= 0) {
+		fclose(in);
+		return -1;
+	}
+
+	FILE *out = fopen(output_file, "wt");
+	hexdump(input_buf, input_size, out, line_size);
+
+	fclose(in);
+	fclose(out);
+
+	return 0;
+}
 
 int main(int argc, char *argv[])
 {
@@ -7,6 +32,7 @@ int main(int argc, char *argv[])
 	char *input_file = NULL;
 	char *output_file = NULL;
 	int err_flag = 0;
+	int line_size = 8;
 
 	while ((opt = getopt (argc, argv, "o:f:h")) != -1) {
 		switch (opt) {
@@ -32,6 +58,8 @@ int main(int argc, char *argv[])
 	}
 	if (err_flag != 0)
 		return err_flag;
+
+	hexdump_to_file(input_file, output_file, line_size);
 
 	return 0;
 }
